9e05bc1bf9f579b9f376158f92c3773fa62d2981
arvinesmaeily <arvinismaili@gmail.com>
2023-06-17 02:51:38 +0330

    - Added Views - Added Navigation Through Tags

--- TypeXercise/MainPage.xaml.cs
+++ TypeXercise/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Controls;
 using muxc=Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,28 +30,59 @@ namespace TypeXercise
             this.InitializeComponent();
         }
 
-        private void NavigationViewItem_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            
-            if (sender != null)
-            {
 
-                muxc.NavigationViewItem navigationViewItem = sender as muxc.NavigationViewItem;
-                var StackPanel = navigationViewItem.Content as StackPanel;
-                var AnimatedVisualPlayer = StackPanel.Children[0] as muxc.AnimatedVisualPlayer;
-                AnimatedVisualPlayer.AutoPlay = true;
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+
+        private void NavigationViewMain_ItemInvoked(muxc.NavigationView sender,
+                                         muxc.NavigationViewItemInvokedEventArgs args)
+        {
+
+            if (args.InvokedItemContainer != null)
+            {
+                Type navPageType = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+                NavigationViewMain_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
             }
         }
 
-        private void NavigationViewItem_PointerExited(object sender, PointerRoutedEventArgs e)
+        private void NavigationViewMain_Navigate(
+            Type navPageType,
+            NavigationTransitionInfo transitionInfo)
         {
-            if (sender != null)
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            Type preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (navPageType != null && !Type.Equals(preNavPageType, navPageType))
             {
-                muxc.NavigationViewItem navigationViewItem = sender as muxc.NavigationViewItem;
-                var StackPanel = navigationViewItem.Content as StackPanel;
-                var AnimatedVisualPlayer = StackPanel.Children[0] as muxc.AnimatedVisualPlayer;
-                AnimatedVisualPlayer.AutoPlay = false;
+                ContentFrame.Navigate(navPageType, null, transitionInfo);
             }
         }
+
+        private void NavigationViewMain_BackRequested(muxc.NavigationView sender,
+                                           muxc.NavigationViewBackRequestedEventArgs args)
+        {
+            TryGoBack();
+        }
+
+        private bool TryGoBack()
+        {
+            if (!ContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavigationViewMain.IsPaneOpen &&
+                (NavigationViewMain.DisplayMode == muxc.NavigationViewDisplayMode.Compact ||
+                 NavigationViewMain.DisplayMode == muxc.NavigationViewDisplayMode.Minimal))
+                return false;
+
+            ContentFrame.GoBack();
+            return true;
+        }
+
     }
 }
